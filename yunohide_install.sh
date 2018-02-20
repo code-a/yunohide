3712047d816c33b1c54307127594957a5f5260ed
nebulak <nebulak@users.noreply.github.com>
2018-02-20 14:24:52 +0100

    colored output

--- yunohide_install.sh
+++ yunohide_install.sh
@@ -1,14 +1,6 @@
 # //TODO: Color output: https://misc.flogisoft.com/bash/tip_colors_and_formatting
 # //TODO: Color echo functions: https://gist.github.com/daytonn/8677243
 
-# //TODO: Close Ports TCP ipv4+ipv6+upnp: 22, 25, 53, 80, 443, 465, 587, 993, 5222, 5269
-# //TODO: Close Ports UDP ipv4+ipv6+upnp: 53, 5353
-# //TODO Example TCP ipv4: yunohost firewall disallow TCP 22 --ipv4-only --no-reload
-# //TODO Example TCP ipv6: yunohost firewall disallow TCP 22 --ipv6-only --no-reload
-# //TODO Example TCP upnp: yunohost firewall disallow TCP 22 --upnp-only --no-reload
-# //TODO: reload firewall after modification:  yunohost firewall reload
-
-# //TODO: change root pw: echo root:$2 | chpasswd
 
 # //TODO: automatic "Y" for tor packet installation
 
@@ -16,8 +8,19 @@
 
 # //TODO: start tor after boot: https://www.raspberrypi.org/forums/viewtopic.php?f=28&t=197052&p=1231906#p1231634
 
-# //TODO: yunohost tools update
-# //TODO: yunohost tools upgrade
+# Echo functions
+RED='\033[0;31m'
+PURPLE="\033[0;35m"
+GREEN="\033[0;32m"
+NC='\033[0m'
+# Print purple
+function echo_n {
+    echo -e "${PURPLE}${1}${NC}"
+}
+
+function echo_green {
+    echo -e "${GREEN}${1}${NC}"
+}
 
 # Get password for admin account
 echo "Enter the password you want to use for your yunohost admin account and the root user"
@@ -31,54 +34,108 @@ if [[ "$PASSWORD" != "$PASSCONFIRM" ]]; then
 fi
 
 # //TODO: change root password
+echo root:$PASSWORD | chpasswd
 
-# //TODO: does this crash yunohost?
+# //TODO: does this crash yunohost? Is it needed?
 # Update package list
-echo "updating package list"
+echo_n "updating package list"
 apt-get update
 
-echo "upgrading packages"
+echo_n "upgrading packages"
 apt-get upgrade
 
+echo_n "Installing apt-transport-https"
+apt-get install apt-transport-https
 
 # Tor installation & hidden service creation
-echo "Installing tor..."
+echo_n "Installing tor..."
 sudo apt-get install tor
 
-echo "Creating hidden service for ssh access..."
+echo_n "Creating hidden service for ssh access..."
 echo '# Hidden service for ssh' >> /etc/tor/torrc
 echo 'HiddenServiceDir  /var/lib/tor/hidden_service_ssh/' >> /etc/tor/torrc
 echo 'HiddenServicePort 22 127.0.0.1:22' >> /etc/tor/torrc
 
-echo "Creating default hidden service for YunoHide..."
+echo_n "Creating default hidden service for YunoHide..."
 echo '# Default hidden service for YunoHide' >> /etc/tor/torrc
 echo 'HiddenServiceDir  /var/lib/tor/hidden_service_default/' >> /etc/tor/torrc
 echo 'HiddenServicePort 80 127.0.0.1:80' >> /etc/tor/torrc
 echo 'HiddenServicePort 443 127.0.0.1:443' >> /etc/tor/torrc
 
-echo "Restarting tor..."
+echo_n "Restarting tor..."
 service tor restart
-echo "waiting for tor to generate hidden services(60s)"
+echo_n "waiting for tor to generate hidden services(60s)"
 sleep 60
 
 hidden_service_ssh="$(cat /var/lib/tor/hidden_service_ssh/hostname)"
 hidden_service_default="$(cat /var/lib/tor/hidden_service_default/hostname)"
 
-echo "Starting YunoHost post-install..."
+echo_n "Starting YunoHost post-install..."
 yunohost tools postinstall -d "$hidden_service_default" -p "$PASSWORD" --ignore-dyndns
 
-# //TODO: update firewall settings
+# update firewall settings
+echo_n "Updating firewall rules..."
+echo_n "Updating firewall rules: IPv4+TCP"
+#yunohost firewall disallow TCP 22 --ipv4-only --no-reload
+yunohost firewall disallow TCP 25 --ipv4-only --no-reload
+yunohost firewall disallow TCP 53 --ipv4-only --no-reload
+yunohost firewall disallow TCP 80 --ipv4-only --no-reload
+yunohost firewall disallow TCP 443 --ipv4-only --no-reload
+yunohost firewall disallow TCP 465 --ipv4-only --no-reload
+yunohost firewall disallow TCP 587 --ipv4-only --no-reload
+yunohost firewall disallow TCP 993 --ipv4-only --no-reload
+yunohost firewall disallow TCP 5222 --ipv4-only --no-reload
+yunohost firewall disallow TCP 5269 --ipv4-only --no-reload
 
-echo "Adding YunoHide AppsList"
-echo "SSH-Address:"
-echo "$hidden_service_ssh"
-echo "YunoHost-Address:"
-echo "$hidden_service_default"
+echo_n "Updating firewall rules: IPv6+TCP"
+#yunohost firewall disallow TCP 22 --ipv6-only --no-reload
+yunohost firewall disallow TCP 25 --ipv6-only --no-reload
+yunohost firewall disallow TCP 53 --ipv6-only --no-reload
+yunohost firewall disallow TCP 80 --ipv6-only --no-reload
+yunohost firewall disallow TCP 443 --ipv6-only --no-reload
+yunohost firewall disallow TCP 465 --ipv6-only --no-reload
+yunohost firewall disallow TCP 587 --ipv6-only --no-reload
+yunohost firewall disallow TCP 993 --ipv6-only --no-reload
+yunohost firewall disallow TCP 5222 --ipv6-only --no-reload
+yunohost firewall disallow TCP 5269 --ipv6-only --no-reload
+
+echo_n "Updating firewall rules: pnpp+TCP"
+#yunohost firewall disallow TCP 22 --pnpp-only --no-reload
+yunohost firewall disallow TCP 25 --pnpp-only --no-reload
+yunohost firewall disallow TCP 53 --pnpp-only --no-reload
+yunohost firewall disallow TCP 80 --pnpp-only --no-reload
+yunohost firewall disallow TCP 443 --pnpp-only --no-reload
+yunohost firewall disallow TCP 465 --pnpp-only --no-reload
+yunohost firewall disallow TCP 587 --pnpp-only --no-reload
+yunohost firewall disallow TCP 993 --pnpp-only --no-reload
+yunohost firewall disallow TCP 5222 --pnpp-only --no-reload
+yunohost firewall disallow TCP 5269 --pnpp-only --no-reload
+
+echo_n "Updating firewall rules: IPv4+UDP"
+yunohost firewall disallow UDP 53 --ipv4-only --no-reload
+yunohost firewall disallow UDP 5353 --ipv4-only --no-reload
+
+echo_n "Updating firewall rules: IPv6+UDP"
+yunohost firewall disallow UDP 53 --ipv6-only --no-reload
+yunohost firewall disallow UDP 5353 --ipv6-only --no-reload
+
+echo_n "Updating firewall rules: upnp+UDP"
+yunohost firewall disallow UDP 53 --upnp-only --no-reload
+yunohost firewall disallow UDP 5353 --upnp-only --no-reload
+
+echo_n "Reloading firewall configuration"
+yunohost firewall reload
+
+
+# //TODO: echo_n "Adding YunoHide AppsList"
+echo_n "SSH-Address:"
+echo_n "$hidden_service_ssh"
+echo_n "YunoHost-Address:"
+echo_n "$hidden_service_default"
 
 # Start update after user has written down the infos
-echo "Please copy and save the addresses shown above."
-echo "You need them to access your server from the internet!"
-echo "In the next step your system will be updated!"
+echo_n "Please copy and save the addresses shown above."
+echo_n "You need them to access your server from the internet!"
 read -rsp $'Press enter to continue...\n'
 
 
