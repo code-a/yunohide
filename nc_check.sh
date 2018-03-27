75bd1f77ee650d382edd738ef20f79d2126e9924
nebulak <nebulak@users.noreply.github.com>
2018-03-27 12:55:21 +0200

    Update nc_check.sh

--- nc_check.sh
+++ nc_check.sh
@@ -1,3 +1,5 @@
 # source: https://docs.nextcloud.com/server/12/admin_manual/configuration_files/encryption_configuration.html
+sudo -u nextcloud php /var/www/nextcloud/console.php maintenance:mode --on
 sudo -u nextcloud php /var/www/nextcloud/console.php encryption:enable
 sudo -u nextcloud php /var/www/nextcloud/console.php encryption:encrypt-all
+sudo -u nextcloud php /var/www/nextcloud/console.php maintenance:mode --off
