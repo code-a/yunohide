fa473abef542c53ef4c8ff0252eeb9249c2b6276
Mischa Buchhofer <buchhofe@pc-stud01.sit.fraunhofer.de>
2018-04-11 12:04:33 +0200

    fix onionconf

--- onionconf.py
+++ onionconf.py
@@ -5,8 +5,8 @@ usage:
     onionconf (-h | --help)
     onionconf hs_list
     onionconf hs_add [--name=<name>] [--path=<path>] [--enabled=<enabled>] [--portmap=<portmap>]
-    onionconf hs_add_auth 
-    
+    onionconf hs_add_auth
+
     onionconf --version
 """
 
@@ -22,9 +22,12 @@ def is_command(s):
     return True
 
 def hs_list():
-    with open("hs_list.json") as json_file:
-        hs_list = json.load(json_file)
-        print(hs_list)
+    try:
+        with open("hs_list.json") as json_file:
+            hs_list = json.load(json_file)
+            print(hs_list)
+    except Exception as e:
+        print(e.message)
 
 def dispatch_command(arguments, command='fail'):
     if command=='hs_list':
@@ -34,15 +37,15 @@ def main():
     arguments = docopt(__doc__)
     version_string = 'alpha'
     arguments = docopt(__doc__, version=version_string)
-    command = get_first([
+    command = list([
         k for (k, v) in arguments.iteritems()
-        if (v and is_command(k))])
+        if (v and is_command(k))])[0]
     try:
         dispatch_command(arguments, command)
     except Exception as e:
         print(e)
 
-            
+
 
 
 """
@@ -51,7 +54,7 @@ def hs_add(name, path, enabled, port_maps, auth_type, auth_info):
     hs['name'] = name
     hs['path'] = path
     hs['enabled'] = enabled
-    hs['port_maps'] = 
+    hs['port_maps'] =
     with open("hs_list.json") as json_file:
         hs_list = json.load(json_file)
 """
