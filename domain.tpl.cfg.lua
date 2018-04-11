f1d5d1a6f66f1bf4c09d111795b04692baea4ec5
nebulak <nebulak@users.noreply.github.com>
2018-04-11 13:05:28 +0200

    Update domain.tpl.cfg.lua

--- domain.tpl.cfg.lua
+++ domain.tpl.cfg.lua
@@ -1,6 +1,7 @@
 VirtualHost "{{ domain }}"
   modules_enabled = {onions}
   onions_only = true
+  onions_tor_all = true
   ssl = {
         key = "/etc/yunohost/certs/{{ domain }}/key.pem";
         certificate = "/etc/yunohost/certs/{{ domain }}/crt.pem";
