6cc350b5a7b3b47b18a7da1f65c8380fab7c2c12
nebulak <nebulak@openmailbox.org>
2018-12-04 10:15:41 +0100

    fix yunohost password

--- yunohide_install.sh
+++ yunohide_install.sh
@@ -42,7 +42,16 @@ function banner {
 ############################## PASSWORD SETUP ####################################
 banner
 
-############################# For VPS ###########################################
+# Get password for admin account
+echo_n "Enter the password you want to use for your yunohost admin account and the root user"
+read -s -p "Password: " PASSWORD; echo
+read -s -p "Confirm Password: " PASSCONFIRM; echo
+
+if [[ "$PASSWORD" != "$PASSCONFIRM" ]]; then
+ echo "Passwords do not match, exiting..."
+ echo "Restart this script and try again!"
+ exit -1
+fi
 
 
 
